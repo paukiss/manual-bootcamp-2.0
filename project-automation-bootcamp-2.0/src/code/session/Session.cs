b5c880b80e3b8fd4d791e03d883385773d933921
sergiopaucara <sergiopaucara@gmail.com>
2023-05-30 00:55:33 -0400

    Create session for Browser

---
+++ project-automation-bootcamp-2.0/src/code/session/Session.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using project_automation_bootcamp_2._0.src.code.browser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_automation_bootcamp_2._0.src.code.session
+{
+    public class Session
+    {
+        private static Session instance = null;
+        private IWebDriver browser;
+
+        private Session()
+        {
+            browser = FactoryBrowser.Make("chrome").Create();
+        }
+
+        public static Session Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Session();
+            }
+
+            return instance;
+        }
+
+        public void CloseBrowser()
+        {
+            instance = null;
+            browser.Quit();
+        }
+
+        public IWebDriver GetBrowser()
+        {
+            return browser;
+        }
+
+    }
+}
